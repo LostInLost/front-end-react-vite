572e0c28b7befc6922577952d247e6579b126e23
BuildTools <unconfigured@null.spigotmc.org>
2023-10-20 21:55:55 +0700

    Form 2 & 3

--- src/Pages/FormSatu.jsx
+++ src/Pages/FormSatu.jsx
@@ -314,6 +314,7 @@ export default function FormSatu() {
                         rounded-md
                         text-white
                         "
+                        type='reset'
                     >
                       Reset
                     </button>
